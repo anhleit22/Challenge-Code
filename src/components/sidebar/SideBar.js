4995f2e0532ef8af1f566d835a2aa0c7ddf7cf1b
anhleit22 <anhleit22@gmail.com>
2025-06-01 18:26:17 +0700

    sidebar active item when route

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import './SideBar.css';
 import { useLogin } from '../../LoginProvider';
 
 function SideBar() {
   const { user } = useLogin();
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   return (
     <div className="sidebar-container">
       <h1>Skipli AI</h1>
       {user && (
-      <div className='container-sidebar'>
-        <div className="item-sidebar"> <span><i className="icon-sidebar fa-solid fa-dice-four"></i></span><span>Services</span></div>
-        <div  className="item-sidebar"> <span><i className="icon-sidebar fa-regular fa-user icon-user"></i></span><span>Profile</span></div>
-      </div>
+        <div className="container-sidebar">
+          <Link
+            to="/"
+            className={`item-sidebar ${currentPath === '/' ? 'active' : ''}`}
+          >
+            <span><i className="icon-sidebar fa-solid fa-dice-four"></i></span>
+            <span>Services</span>
+          </Link>
+
+          <Link
+            to="/profile"
+            className={`item-sidebar ${currentPath === '/profile' ? 'active' : ''}`}
+          >
+            <span><i className="icon-sidebar fa-regular fa-user icon-user"></i></span>
+            <span>Profile</span>
+          </Link>
+        </div>
       )}
     </div>
   );
